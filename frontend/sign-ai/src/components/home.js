d4fc0ebf4a9208e8c9ddc3194f3bb942aa43974b
RaasheedaFathima <64429364+RaasheedaFathima@users.noreply.github.com>
2021-10-21 16:38:15 +0800

    blob shape

--- frontend/sign-ai/src/components/home.js
+++ frontend/sign-ai/src/components/home.js
@@ -3,24 +3,31 @@ import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
 import logo from '../logo.png'
 import "./home.css";
+import { RandomBlob, RandomHLine } from 'react-random-shapes'
 
 console.log(logo); // /logo.84287d09.png
 
 function Home(){
     
         return(
+            
             <div>
-                <img className="photo" src={logo} alt="Logo"/>
                 <div className="intro">
+                <img className="photo" src={logo} alt="Logo"/>
                     <h2>HELLO! </h2>
                     <h3>We will be using SignOn.ai for effective communication between you and our HomeTeam Officer!</h3>
                     <h3>This will be done via a transcription service through the recording of your signing.</h3>
                     <h3>After reading this Please show a thumbs up to go on to the tutorial</h3>
+                    <Button 
+                className="NextHome" 
+                variant="contained" 
+                href="/consent"
+                size="small" >
+                    Next
+                </Button>
                 </div>
                 
-                <Button className="NextHome" variant="contained" href="/consent">
-                Next
-      </Button>
+                
         </div>
             
             
