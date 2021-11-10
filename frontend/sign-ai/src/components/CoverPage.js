6e7b30561bd15f5096ae3d5bef424d226c2cc7d8
RaasheedaFathima <64429364+RaasheedaFathima@users.noreply.github.com>
2021-11-10 20:42:50 +0800

    logo refining 1

--- frontend/sign-ai/src/components/CoverPage.js
+++ frontend/sign-ai/src/components/CoverPage.js
@@ -2,6 +2,7 @@ import React from "react";
 import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
 import logo from '../logo.png'
+import background from "../placeholder.png";
 import { useHistory } from "react-router";
 import "./coverPage.css";
 
@@ -14,7 +15,7 @@ function CoverPage(){
     }
         return(
             
-            <div>
+            <div style={{ backgroundImage: `url(${background})` }}>
                 <div className="intro">
                 <img className="photo" src={logo} alt="Logo"/>
                     <h2>HELLO! </h2>
--- frontend/sign-ai/src/components/tutorial.js
+++ frontend/sign-ai/src/components/tutorial.js
@@ -1,14 +1,16 @@
 import React from "react";
 import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
-import {useHistory} from 'react-router-dom'; 
+import {useHistory} from 'react-router-dom';
+import background from "../placeholder.png";
+
 function Tutorial(){
     const history = useHistory()
     function onClick(){
         history.push('/translate')
     }
         return(
-            <div>
+            <div style={{ backgroundImage: `url(${background})` }}>
                 <h1>Tutorial </h1>
                 <h3>To use SignOn.ai, start signing when the HomeTeam Officer shows a thumbs up.</h3>
                 <h3>Once done, Show a thumbs up and hold for 3 seconds to mark the end of the signing</h3>
