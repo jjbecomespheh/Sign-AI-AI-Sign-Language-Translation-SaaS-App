16b3c6645414678daada133a174372dc0203a411
SkyLee <jetxuen_lee@mymail.sutd.edu.sg>
2021-11-11 16:22:46 +0800

    refine the selenium window size and test cases

--- frontend/sign-ai/src/features/step_definitions/selenium-test.js
+++ frontend/sign-ai/src/features/step_definitions/selenium-test.js
@@ -20,32 +20,17 @@ After(async function(){
     await driver.quit()
 })
 
-Given('I am on the Google search page', async function () {
-    await driver.get('http://www.google.com');
-});
-
-When('I search for {string}', async function (searchTerm) {
-    await driver.findElement(By.name('q')).sendKeys(searchTerm+'\n');
-});
-
-Then('the page title should start with {string}', async function (searchTerm) {
-    const title = await driver.getTitle();
-    const isTitleStartWithCheese = title.toLowerCase().lastIndexOf(`${searchTerm}`, 0) === 0;
-    expect(isTitleStartWithCheese).to.equal(true);
-});
-
 Given('I am on the chat history page', async function () {
     // Write code here that turns the phrase above into concrete actions
     await driver.get(url+'chat-history');
     await driver.sleep(3*1000)
-
 });
 
 When('I click on the button with date', async function () {
     // Write code here that turns the phrase above into concrete actions
     const chat_button = await driver.findElement(By.id("conv_id"));
     await chat_button.click();
-    await driver.sleep(3*1000)
+    await driver.sleep(3*1000);
 });
 
 Then('i should be able to see the conversation history', async function () {
@@ -60,11 +45,11 @@ Then('i should be able to see the conversation history', async function () {
 Given('the user navigates to the translate page', async function () {
     // Write code here that turns the phrase above into concrete actions
     driver.get(url+'translate');
+    await driver.sleep(3*1000);
 });
 
 When('he signs {string}', async function (string) {
     // Write code here that turns the phrase above into concrete actions
-    await driver.sleep(6*1000)
     if (string == "successfully"){
         const translate_button = await driver.findElement(By.id("correct_btn"));
 
@@ -85,20 +70,21 @@ Then('he should be notified of {string} translation', async function (string) {
     if(string == "successful"){
         const translate_button = await driver.findElement(By.id("correct_btn"));
         await translate_button.click();
+        await driver.sleep(3*1000)
         await driver.switchTo().alert().accept(); //catch alert
     }else if (string == "failed"){
         const translate_button = await driver.findElement(By.id("sign_again_btn"));
         await translate_button.click();
+        await driver.sleep(3*1000)
         await driver.switchTo().alert().accept(); //catch alert
     }
-    
-    driver.sleep(6*1000)
 });
 
 When('he clicks on the end convo page button',async function () {
     // Write code here that turns the phrase above into concrete actions
     const home_button = await driver.findElement(By.id("home_btn"));
     await home_button.click()
+    await driver.sleep(3*1000)
 });
 
 Then('he should be redirected back to home page',async function () {
@@ -113,6 +99,7 @@ When('he clicks on the ask question button', async function () {
     // Write code here that turns the phrase above into concrete actions
     const ask_button = await driver.findElement(By.id("ask_btn"));
     await ask_button.click()
+    await driver.sleep(3*1000)
 });
 
 Then('he should be redirected to the page', async function() {
