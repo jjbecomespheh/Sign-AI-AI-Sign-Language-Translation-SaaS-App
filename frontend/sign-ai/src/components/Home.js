ae1b62fc206c685bda20dc17ea3a424a1954a3b9
RaasheedaFathima <64429364+RaasheedaFathima@users.noreply.github.com>
2021-11-11 15:49:04 +0800

    Revert "Merge branch 'main' of https://github.com/Service-Design-Studio/1d-final-project-team-4"
    This reverts commit a56a2faf0af610ef8cc74e5413a87980e29c1458, reversing
    changes made to 9ecaef570566fae4f9b2532465607ae11acecf23.

--- frontend/sign-ai/src/components/Home.js
+++ frontend/sign-ai/src/components/Home.js
@@ -6,8 +6,6 @@ import {useHistory} from 'react-router-dom';
 import HistoryIcon from '@mui/icons-material/History';
 import TranslateIcon from '@mui/icons-material/Translate';
 import HelpIcon from '@mui/icons-material/Help';
-import { v4 as uuidv4 } from 'uuid';
-import {store, useGlobalState} from 'state-pool';
 //import Camera, { FACING_MODES, IMAGE_TYPES } from 'react-html5-camera-photo';
 
 function Home(){
@@ -18,10 +16,7 @@ function Home(){
     }
 
     function goTranslate(){
-        var convo_id = uuidv4(); 
-        store.setState("conversation_id", convo_id); 
         history.push('/translate')
-  
     }
 
     function goCoverPage(){
@@ -35,27 +30,21 @@ function Home(){
                 <h3>To start using Sign.ai</h3>
                 <h3>Click on Cover page button</h3> */}
                 <Button 
-                className="NextHome" 
-                variant="contained"
                 onClick={goCoverPage} 
                 tartIcon={<HelpIcon />} 
-                style={{backgroundColor: '#2c7973', color: '#FFFFFF', borderRadius: '12px', margin: '2px',marginTop: '10px', width: '275px', height: '60px', position:'relative'}}
+                style={{backgroundColor: '#4C7031', color: '#FFFFFF', borderRadius: '12px', margin: '2px',marginTop: '10px', width: '275px', height: '60px', position:'relative'}}
                 >Cover Page</Button>
                 
                 <Button 
-                    className="NextHome" 
-                    variant="contained"
                     onClick={goTranslate} 
                     startIcon={<TranslateIcon />} 
-                    style={{backgroundColor: '#f7b34d', color: '#FFFFFF', borderRadius: '12px', margin: '2px', marginTop: '15px', width: '275px', height: '60px', position:'relative'}}
-                    >New Conversation</Button>
+                    style={{backgroundColor: '#4C7031', color: '#FFFFFF', borderRadius: '12px', margin: '2px', marginTop: '15px', width: '275px', height: '60px', position:'relative'}}
+                    >Translate</Button>
                 
                 <Button 
-                    className="NextHome" 
-                    variant="contained"
                     onClick={goChatHistory} 
                     startIcon={<HistoryIcon />}
-                    style={{backgroundColor: '#2c7973', color: '#FFFFFF', borderRadius: '12px', margin: '2px', marginTop: '15px', width: '275px', height: '60px', position:'relative'}}
+                    style={{backgroundColor: '#4C7031', color: '#FFFFFF', borderRadius: '12px', margin: '2px', marginTop: '15px', width: '275px', height: '60px', position:'relative'}}
                     >Chat History</Button>
             </div>
         )
--- frontend/sign-ai/src/components/consent.js
+++ frontend/sign-ai/src/components/consent.js
@@ -12,25 +12,10 @@ function Consent(){
         return(
             <div>
                 <h1>Consent </h1>
-                {/* <h3>We will like to inform you that your statement will be recorded via the HomeTeam Officers camera.</h3>
+                <h3>We will like to inform you that your statement will be recorded via the HomeTeam Officers camera.</h3>
                 <h3>All recordings will be kept confidential and will solely be used for law and training purposes.</h3>
                 <h3>Show a THUMBS UP if you consent</h3>
-                <h3>Show a THUMBS DOWN if you do not consent</h3> */}
-                <div style={{
-                            position: 'relative', left: '50%', top: '50%',
-                            transform: 'translate(-50%, -50%)',
-                            marginTop: '150px'}}>
-                    <Button
-                        id="translated_text"
-                        className="NextHome" 
-                        variant="contained"
-                        style={{width: 350, height: 250, backgroundColor: '#f7b34d', borderRadius: '12px', color: '#000000'}}
-                        
-                        >We will like to inform you that your statement will be recorded via the HomeTeam Officers camera. All recordings will be kept confidential and will solely be used for law and training purposes.</Button>
-                </div>
-                <h3>Show a thumbs up if you consent</h3>
-                <h3>Show a thumbs down if you do not consent</h3>
-
+                <h3>Show a THUMBS DOWN if you do not consent</h3>
                 <Button className="NextHome" 
                     variant="contained"
                     style={{backgroundColor: '#F49619', color: '#FFFFFF', borderRadius: '15px', margin: '2px', marginTop: '10px'}}
