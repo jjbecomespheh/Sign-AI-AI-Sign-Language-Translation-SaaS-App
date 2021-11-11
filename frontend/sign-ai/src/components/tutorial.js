9ecaef570566fae4f9b2532465607ae11acecf23
RaasheedaFathima <64429364+RaasheedaFathima@users.noreply.github.com>
2021-11-11 15:40:03 +0800

    ui changes to tutorial

--- frontend/sign-ai/src/components/tutorial.js
+++ frontend/sign-ai/src/components/tutorial.js
@@ -2,7 +2,6 @@ import React from "react";
 import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
 import {useHistory} from 'react-router-dom';
-import background from "../placeholder.png";
 
 function Tutorial(){
     const history = useHistory()
@@ -10,7 +9,7 @@ function Tutorial(){
         history.push('/translate')
     }
         return(
-            <div style={{ backgroundImage: `url(${background})` }}>
+            <div>
                 <h1>Tutorial </h1>
                 <h3>To use SignOn.ai, start signing when the HomeTeam Officer shows a thumbs up.</h3>
                 <h3>Once done, Show a thumbs up and hold for 3 seconds to mark the end of the signing</h3>
