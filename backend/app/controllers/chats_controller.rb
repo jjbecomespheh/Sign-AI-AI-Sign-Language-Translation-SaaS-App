feca4fab3eeaee9308c5414fa13419a1f95746a2
SkyLee <jetxuen_lee@mymail.sutd.edu.sg>
2021-11-17 10:52:54 +0800

    disable csrf token

--- backend/app/controllers/chats_controller.rb
+++ backend/app/controllers/chats_controller.rb
@@ -1,4 +1,5 @@
 class ChatsController < ApplicationController
+  protect_from_forgery with: :null_session
   before_action :set_chat, only: %i[ show edit update destroy ]
 
   # GET /chats or /chats.json
