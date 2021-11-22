ff78294015cf5347a26e54713938946cab991402
Anirudh181001 <shrivijiani@gmail.com>
2021-11-23 02:20:34 +0800

    requirements changed

--- AI/SimpleAILife/Code/realtime_testing.py
+++ AI/SimpleAILife/Code/realtime_testing.py
@@ -17,10 +17,8 @@ cap = cv2.VideoCapture(0)
 
 with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
     while cap.isOpened():
-
-
+        
         ret, frame = cap.read()
-
 
         image, results = mediapipe_detection(frame, holistic)
         # print(results)
