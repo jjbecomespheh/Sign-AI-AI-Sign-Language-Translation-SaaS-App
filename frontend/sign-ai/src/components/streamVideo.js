f1b93bdaeb887a82c17dfa6e8422ca80710d70c7
Lim Hng Yi <limhngyi@rocketmail.com>
2021-12-09 15:40:58 +0800

    final working model

--- frontend/sign-ai/src/components/streamVideo.js
+++ frontend/sign-ai/src/components/streamVideo.js
@@ -14,7 +14,7 @@ export default function StreamVideo() {
   const [loaded, setLoaded] = useState(false);
 
   useEffect(() => {
-    classifier = ml5.imageClassifier("./model/tm_my_image_model/model.json", () => {
+    classifier = ml5.imageClassifier("./model/tm_my_image_model_2/model.json", () => {
       navigator.mediaDevices
         .getUserMedia({ video: true, audio: false })
         .then((stream) => {
